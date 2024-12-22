ac0b2aac6242dbaae70a12b263fb1ed019fc764c
KartikShrii1910 <kartikmandloi1020@gmail.com>
2024-12-22 13:04:27 +0530

    test for uapdates

--- src/main/java/com/coursemanagement/response/JwtAuthenticationResponse.java
+++ src/main/java/com/coursemanagement/response/JwtAuthenticationResponse.java
@@ -1,45 +1,27 @@
 package com.coursemanagement.response;
 
-
-
 public class JwtAuthenticationResponse {
 
-    private String accessToken;
-    private String tokenType = "Bearer";
-//    private String role;
+	private String accessToken;
+	private String tokenType = "Bearer";
 
-    public JwtAuthenticationResponse(String accessToken) {
-        this.accessToken = accessToken;
-    }
+	public JwtAuthenticationResponse(String accessToken) {
+		this.accessToken = accessToken;
+	}
 
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-//    public JwtAuthenticationResponse(String accessToken, String role) {
-//		super();
-//		this.accessToken = accessToken;
-//		this.role = role;
-//	}
+	public String getAccessToken() {
+		return accessToken;
+	}
 
 	public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-//    public String getRole() {
-//		return role;
-//	}
-//
-//	public void setRole(String role) {
-//		this.role = role;
-//	}
+		this.accessToken = accessToken;
+	}
 
 	public String getTokenType() {
-        return tokenType;
-    }
+		return tokenType;
+	}
 
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
 }
- 
